a8f64e8a3f00ee96fef662cc6e930c00b921aa6a
Marc <meq.1515@gmail.com>
2023-12-12 20:34:43 +1100

    I deleted the commented import because I think it will not be necessary

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -5,12 +5,12 @@ function HeaderComponent() {
 		<>
 			<header className='header'>
 				<nav className='header__nav nav'>
-					<ul className='nav__ul'>
-						<li className='nav__li'>Marc</li>
-						<li className='nav__li'>About Me</li>
-						<li className='nav__li'>Technologies</li>
-						<li className='nav__li'>Projects</li>
-						<li className='nav__li'>Contact</li>
+					<ul className='nav nav__ul'>
+						<li className='nav nav__li'>Marc</li>
+						<li className='nav nav__li'>About Me</li>
+						<li className='nav nav__li'>Technologies</li>
+						<li className='nav nav__li'>Projects</li>
+						<li className='nav nav__li'>Contact</li>
 					</ul>
 				</nav>
 			</header>
