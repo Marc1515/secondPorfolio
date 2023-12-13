56c609ef79249aac09209b8f38001ec5bbecaf23
Marc <meq.1515@gmail.com>
2023-12-13 17:42:34 +1100

    Provieder from the new context wrapping the main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { MenuProvider } from './context/MenuContext';
 import HeaderComponent from './components/header/HeaderComponent';
 
 function App() {
 	return (
-		<>
+		<MenuProvider>
 			<HeaderComponent />
-		</>
+		</MenuProvider>
 	);
 }
 
