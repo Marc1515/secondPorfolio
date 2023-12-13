23c64720201734ef365702837162b2065f41fa5d
Marc <meq.1515@gmail.com>
2023-12-13 17:48:03 +1100

    BurguerButtonComponent implemented only if the screen width is under 480px

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState, useContext } from 'react';
+import { MenuContext } from './../../context/MenuContext';
+import BurguerButtonComponent from './burguerButton/BurguerButtonComponent';
 import './headerStyles.scss';
 
 function HeaderComponent() {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	const { menuOpen, toggleMenu } = useContext(MenuContext);
+
+	useEffect(() => {
+		const handleResize = () => setWindowWidth(window.innerWidth);
+		window.addEventListener('resize', handleResize);
+
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
 	return (
 		<>
 			<header className='header'>
-				<nav className='header__nav nav'>
+				{windowWidth <= 480 && (
+					<BurguerButtonComponent toggleMenu={toggleMenu} />
+				)}
+				<nav className={`header__nav nav ${menuOpen ? '' : 'hidden'}`}>
 					<ul className='nav nav__ul'>
 						<li className='nav nav__li'>Marc</li>
 						<li className='nav nav__li'>About Me</li>
