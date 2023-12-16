964c8e5d6504a347812aa4698ffdc8e4557a6b25
Marc <meq.1515@gmail.com>
2023-12-16 11:12:39 +1100

    At the end I decided to take another way to do the animation on the navbar, because I didn't find how to do it with a resposponsive way

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -14,32 +14,6 @@ function HeaderComponent() {
 		return () => window.removeEventListener('resize', handleResize);
 	}, []);
 
-	useEffect(() => {
-		const navLinks = document.querySelectorAll('.header__link');
-		const underline = document.querySelector('.header__underline');
-
-		const handleMouseEnter = (link) => {
-			underline.style.width = `${link.offsetWidth}px`;
-			underline.style.left = `${link.offsetLeft}px`;
-		};
-
-		const handleMouseLeave = () => {
-			underline.style.width = 0;
-		};
-
-		navLinks.forEach((link) => {
-			link.addEventListener('mouseenter', () => handleMouseEnter(link));
-			link.addEventListener('mouseleave', handleMouseLeave);
-		});
-
-		return () => {
-			navLinks.forEach((link) => {
-				link.removeEventListener('mouseenter', () => handleMouseEnter(link));
-				link.removeEventListener('mouseleave', handleMouseLeave);
-			});
-		};
-	}, []);
-
 	return (
 		<>
 			<header className='header'>
@@ -49,22 +23,31 @@ function HeaderComponent() {
 				<nav className={`header__nav ${menuOpen ? '' : 'hidden'}`}>
 					<ul className='header__list'>
 						<li className='header__item'>
-							<a className='header__link'>Marc</a>
+							<a className='header__link'>
+								Marc<span></span>
+							</a>
 						</li>
 						<li className='header__item'>
-							<a className='header__link'>About Me</a>
+							<a className='header__link'>
+								About Me<span></span>
+							</a>
 						</li>
 						<li className='header__item'>
-							<a className='header__link'>Technologies</a>
+							<a className='header__link'>
+								Technologies<span></span>
+							</a>
 						</li>
 						<li className='header__item'>
-							<a className='header__link'>Projects</a>
+							<a className='header__link'>
+								Projects<span></span>
+							</a>
 						</li>
 						<li className='header__item'>
-							<a className='header__link'>Contact</a>
+							<a className='header__link'>
+								Contact<span></span>
+							</a>
 						</li>
 					</ul>
-					<div className='header__underline'></div> {/* Agregar esto */}
 				</nav>
 			</header>
 		</>
