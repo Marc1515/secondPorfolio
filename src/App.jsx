1d86c5e150c747c8ac638e162d3aa1848a0634b0
Marc1515 <meq.1515@gmail.com>
2023-12-28 10:35:18 +1100

    AboutComponent added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { MenuProvider } from './context/MenuContext';
 import HeaderComponent from './components/header/HeaderComponent';
 import MarcComponent from './components/marc/MarcComponent';
-import './styles/_base.scss'
+import AboutComponent from './components/about/aboutComponent';
+import './styles/_base.scss';
 
 function App() {
 	return (
 		<MenuProvider>
 			<HeaderComponent />
 			<MarcComponent />
+			<AboutComponent />
 		</MenuProvider>
 	);
 }
