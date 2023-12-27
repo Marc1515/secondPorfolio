1fa8b368f09ca6e887bac91de83af768190e79e9
Marc1515 <meq.1515@gmail.com>
2023-12-27 21:08:04 +1100

    New Component added to the main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { MenuProvider } from './context/MenuContext';
 import HeaderComponent from './components/header/HeaderComponent';
+import MarcComponent from './components/marc/MarcComponent';
+import './styles/_base.scss'
 
 function App() {
 	return (
 		<MenuProvider>
 			<HeaderComponent />
+			<MarcComponent />
 		</MenuProvider>
 	);
 }
