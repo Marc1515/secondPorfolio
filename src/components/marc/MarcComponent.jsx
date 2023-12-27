84a8b7fd958b6b6bd5f58cd53362c48a00977ee8
Marc1515 <meq.1515@gmail.com>
2023-12-27 21:07:19 +1100

    New component created to show the main part of the porfolio

---
+++ src/components/marc/MarcComponent.jsx
@@ -0,0 +1,51 @@
+import { useEffect } from 'react';
+import Typed from 'typed.js';
+import fotoPerfil from './../../assets/img/fotoPerfil.png';
+import CV from './../../assets/MEQ.pdf';
+import './MarcComponent.scss';
+
+const MarcComponent = () => {
+	useEffect(() => {
+		const options = {
+			strings: ['BIENVENIDO A MI PORTFOLIO COMO DESARROLLADOR WEB'],
+			typeSpeed: 40,
+			backSpeed: 10,
+			loop: true,
+			showCursor: false, // Opción para ocultar el cursor
+		};
+		const typed = new Typed('#typed-heading', options);
+		return () => {
+			typed.destroy();
+		};
+	}, []);
+
+	return (
+		<>
+			<section>
+				<div className='imgContainer'>
+					{<img src={fotoPerfil} alt='' className='imgContainer__Img' />}
+				</div>
+				<div className='contentWrapper'>
+					<div className='titleContainer'>
+						<span className='titleContainer__FullName'>
+							<span className='titleContainer__Name'>Marc</span>España
+						</span>
+					</div>
+
+					<div className='autoTypedContainer'>
+						<h4 className='autoTypedContainer__Text' id='typed-heading'></h4>
+					</div>
+					<div className='buttonContainer'>
+						<button className='buttonContainer__Button'>
+							<a className='buttonContainer__Link' href={CV} download>
+								Descarga mi CV
+							</a>
+						</button>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default MarcComponent;
