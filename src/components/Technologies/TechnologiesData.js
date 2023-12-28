f8c1885888ab89e2a4638dc9a3018d136c4efd6e
Marc1515 <meq.1515@gmail.com>
2023-12-28 19:09:03 +1100

    The data I need to show on the technologies section

---
+++ src/components/Technologies/TechnologiesData.js
@@ -0,0 +1,43 @@
+import htmlSvgIMG from './../../assets/img/techSVG/html5-color.svg';
+import cssSvgIMG from './../../assets/img/techSVG/css3-color.svg';
+import sassSvgIMG from './../../assets/img/techSVG/sass-color.svg';
+import javaScriptSvgIMG from './../../assets/img/techSVG/javascript-color.svg';
+import typeScriptSvgIMG from './../../assets/img/techSVG/typescript-color.svg';
+import angularSvgIMG from './../../assets/img/techSVG/angular-color.svg';
+import rxjsSvgIMG from './../../assets/img/techSVG/reactivex-color.svg';
+import reactSvgIMG from './../../assets/img/techSVG/react-color.svg';
+import wordpressSvgIMG from './../../assets/img/techSVG/wordpress-color.svg';
+import wooCommerceSvgIMG from './../../assets/img/techSVG/woo-color.svg';
+import bootstrapSvgIMG from './../../assets/img/techSVG/bootstrap-color.svg';
+import tailwindSvgIMG from './../../assets/img/techSVG/tailwindcss-color.svg';
+
+const TechImages = {
+	techBuild: {
+		htmlSVG: htmlSvgIMG,
+		cssSVG: cssSvgIMG,
+        sassSVG: sassSvgIMG,
+		javaScriptSvg: javaScriptSvgIMG,
+		angularSVG: angularSvgIMG,
+		rxjsSvg: rxjsSvgIMG,
+		typeScriptSvg: typeScriptSvgIMG,
+		reactSVG: reactSvgIMG,
+		wordpressSVG: wordpressSvgIMG,
+		wooCommerceSVG: wooCommerceSvgIMG,
+		bootstrapSVG: bootstrapSvgIMG,
+		tailwindSvg: tailwindSvgIMG,
+	},
+
+	techDescriptions: {
+		htmlCssJs:
+			'My experience in HTML allows me to effectively structure web content. Through CSS, I apply responsive and attractive designs, and with JavaScript, I manage dynamic interactions and API manipulation in my web projects.',
+		tailwindBootstrap:
+			'Utilizo Bootstrap para construir rápidamente interfaces de usuario responsivas con sus componentes predefinidos. Tailwind, en cambio, lo uso cuando necesito un control granular sobre el estilo, ya que proporciona una gran cantidad de clases utilitarias para construir diseños personalizados.',
+		typescriptRxjs:
+			'Como desarrollador entiendo como funciona el superset Typescript, que me permite escribir código más seguro y predecible gracias a su tipado fuerte. También tengo habilidades sólidas con RxJS, una biblioteca de programación reactiva que utilizo para manejar secuencias de eventos asíncronos con los operadores, permitiéndome un flujo de datos más eficiente y escalables.',
+		reactAngular:
+			'Tengo un sólido entendimiento tanto de React como de Angular. Con React, puedo crear interfaces de usuario dinámicas y eficientes, aprovechando su modelo de componentes, la reconciliación de DOM virtual y el uso de los hooks. En Angular, manejo la creación de aplicaciones de página única a gran escala, utilizando su completo marco basado en TypeScript, con una profunda comprensión de los módulos, componentes, servicios, y enrutamiento.',
+		wordpressWoocommerce:
+			'Tengo experiencia en la plataforma de gestión WordPress, para crear sitios web atractivos y funcionales. Adicionalmente, estoy familiarizado con WooCommerce que me permite la creación de tiendas online robustas y personalizables, incluyendo configuración de productos, métodos de pago, y gestión de envíos.',
+	},
+};
+export default TechImages;
