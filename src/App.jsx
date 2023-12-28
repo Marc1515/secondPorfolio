28a37551ebba1294efdff4c12e378dca2e20ccbb
Marc1515 <meq.1515@gmail.com>
2023-12-28 13:03:20 +1100

    As I need to do some test I added the new components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { MenuProvider } from './context/MenuContext';
 import HeaderComponent from './components/header/HeaderComponent';
 import MarcComponent from './components/marc/MarcComponent';
 import AboutComponent from './components/about/aboutComponent';
+import TechnologiesComponent from './components/Technologies/TechnologiesComponent';
+import ProjectsComponent from './components/Projects/ProjectsComponent';
+import ContactComponent from './components/contact/ContactComponent';
 import './styles/_base.scss';
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
 			<HeaderComponent />
 			<MarcComponent />
 			<AboutComponent />
+			<TechnologiesComponent />
+			<ProjectsComponent />
+			<ContactComponent />
 		</MenuProvider>
 	);
 }
