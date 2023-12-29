513ff9710b5abe22b73c13ee96a0a38a5d839dc1
Marc1515 <meq.1515@gmail.com>
2023-12-29 17:08:52 +1100

    Beginnings about contact component structure setted

--- src/components/Contact/ContactComponent.jsx
+++ src/components/Contact/ContactComponent.jsx
@@ -1,10 +1,60 @@
+import { MdOutlineMail } from 'react-icons/md';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { BsWhatsapp } from 'react-icons/bs';
 import './ContactComponent.scss';
 
 const ContactComponent = () => {
 	return (
 		<>
 			<section id='contact'>
-				<h1>Contact</h1>
+				<div className='contactTitle'>
+					<span className='contactTitle__Title'>Contact</span>
+				</div>
+				<div className='contactCards'>
+					<div className='contactCards__Card'>
+						<BsWhatsapp className='contactCards__Icon' />
+						<span className='contactCards__IconName'>WhatsApp</span>
+						<span className='contactCards__Info'>0451 565 137</span>
+						<a
+							className='contactCards__Link'
+							href='whatsapp://send?phone=+61451565137'
+							target='_blank'
+							rel='noreferrer'
+						>
+							Send a message
+						</a>
+					</div>
+				</div>
+				<div className='contactCards'>
+					<div className='contactCards__Card'>
+						<MdOutlineMail className='contactCards__Icon' />
+						<span className='contactCards__IconName'>Mail</span>
+						<span className='contactCards__Info'>meq.1515@gmail.com</span>
+						<a
+							className='contactCards__Link'
+							href='mailto:meq.1515@gmail.com'
+							target='_blank'
+							rel='noreferrer'
+						>
+							Send a message
+						</a>
+					</div>
+				</div>
+				<div className='contactCards'>
+					<div className='contactCards__Card'>
+						<AiFillLinkedin className='contactCards__Icon' />
+						<span className='contactCards__IconName'>LinkedIn</span>
+						<span className='contactCards__Info'>Marc España</span>
+						<a
+							className='contactCards__Link'
+							href='https://www.linkedin.com/in/marc-espa%C3%B1a-833924141/'
+							target='_blank'
+							rel='noreferrer'
+						>
+							Send a message
+						</a>
+					</div>
+				</div>
 			</section>
 		</>
 	);
