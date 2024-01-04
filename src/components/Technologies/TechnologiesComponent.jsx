e1e4bf6a18a7092ba16b4453a8d71286ddc0d776
Marc1515 <meq.1515@gmail.com>
2024-01-04 18:47:04 +1100

    New Technologies learned added and some styles done to fit better in wider screens

--- src/components/Technologies/TechnologiesComponent.jsx
+++ src/components/Technologies/TechnologiesComponent.jsx
@@ -23,176 +23,211 @@ const TechnologiesComponent = () => {
 						technologies.
 					</span>
 				</div>
-				{/* Basic Technologies */}
-				<div className='TechCard'>
-					<span className='TechCard__Tittle'>Basic</span>
-					<div className='TechCard__WrapperImgs'>
-						<img
-							className='TechCard__Img'
-							src={TechImages.techBuild.htmlSVG}
-							alt='html_img'
-						/>
-						<img
-							className='TechCard__Img'
-							src={TechImages.techBuild.cssSVG}
-							alt='css_img'
-						/>
-						<img
-							className='TechCard__Img'
-							src={TechImages.techBuild.javaScriptSvg}
-							alt='javaScript_img'
-						/>
-					</div>
-					<div
-						className={`TechCard__DropDown ${
-							openSection === 'BT' ? 'open' : ''
-						}`}
-						onClick={() => toggleSection('BT')}
-					>
-						<div className='TechCard__DropDownTittle'>
-							<span className='summary'>HTML, CSS y JavaScript</span>
-							<span className='TechCard__ArrowIcon'>
-								{openSection === 'BT' ? <FiChevronUp /> : <FiChevronDown />}
-							</span>
+				<div className='TechCardsWrapper'>
+					{/* Basic Technologies */}
+					<div className='TechCard'>
+						<span className='TechCard__Tittle'>Basic</span>
+						<div className='TechCard__WrapperImgs'>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.htmlSVG}
+								alt='html_img'
+							/>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.cssSVG}
+								alt='css_img'
+							/>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.javaScriptSvg}
+								alt='javaScript_img'
+							/>
 						</div>
-						<div className='TechCard__Text'>
-							{TechImages.techDescriptions.htmlCssJs}
+						<div
+							className={`TechCard__DropDown ${
+								openSection === 'BT' ? 'open' : ''
+							}`}
+							onClick={() => toggleSection('BT')}
+						>
+							<div className='TechCard__DropDownTittle'>
+								<span className='summary'>HTML, CSS y JavaScript</span>
+								<span className='TechCard__ArrowIcon'>
+									{openSection === 'BT' ? <FiChevronUp /> : <FiChevronDown />}
+								</span>
+							</div>
+							<div className='TechCard__Text'>
+								{TechImages.techDescriptions.htmlCssJs}
+							</div>
 						</div>
 					</div>
+					{/* Basic Technologies */}
+					{/* CSS Libraries */}
+					<div className='TechCard'>
+						<span className='TechCard__Tittle'>CSS Libraries</span>
+						<div className='TechCard__WrapperImgs'>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.bootstrapSVG}
+								alt='bootstrap_img'
+							/>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.tailwindSvg}
+								alt='tailwind_img'
+							/>
+						</div>
+						<div
+							className={`TechCard__DropDown ${
+								openSection === 'CSST' ? 'open' : ''
+							}`}
+							onClick={() => toggleSection('CSST')}
+						>
+							<div className='TechCard__DropDownTittle'>
+								<span className='summary'>Bootstrap & Taildwind</span>
+								<span className='TechCard__ArrowIcon'>
+									{openSection === 'CSST' ? <FiChevronUp /> : <FiChevronDown />}
+								</span>
+							</div>
+							<div className='TechCard__Text'>
+								{TechImages.techDescriptions.tailwindBootstrap}
+							</div>
+						</div>
+					</div>
+					{/* CSS Libraries */}
+					{/* Pre-Processors */}
+					<div className='TechCard'>
+						<span className='TechCard__Tittle'>Pre-Processors</span>
+						<div className='TechCard__WrapperImgs'>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.sassSVG}
+								alt='sass_img'
+							/>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.lessSvg}
+								alt='less_img'
+							/>
+						</div>
+						<div
+							className={`TechCard__DropDown ${
+								openSection === 'TPT' ? 'open' : ''
+							}`}
+							onClick={() => toggleSection('TPT')}
+						>
+							<div className='TechCard__DropDownTittle'>
+								<span className='summary'>SASS & LESS</span>
+								<span className='TechCard__ArrowIcon'>
+									{openSection === 'TPT' ? <FiChevronUp /> : <FiChevronDown />}
+								</span>
+							</div>
+							<div className='TechCard__Text'>
+								{TechImages.techDescriptions.sassLess}
+							</div>
+						</div>
+					</div>
+					{/* Pre-Processors */}
+					{/* TypeScript & RxJS */}
+					<div className='TechCard'>
+						<span className='TechCard__Tittle'>Superset & RxJS</span>
+						<div className='TechCard__WrapperImgs'>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.typeScriptSvg}
+								alt='react_img'
+							/>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.rxjsSvg}
+								alt='angular_img'
+							/>
+						</div>
+						<div
+							className={`TechCard__DropDown ${
+								openSection === 'SRT' ? 'open' : ''
+							}`}
+							onClick={() => toggleSection('SRT')}
+						>
+							<div className='TechCard__DropDownTittle'>
+								<span className='summary'>TypeScript & RxJS</span>
+								<span className='TechCard__ArrowIcon'>
+									{openSection === 'SRT' ? <FiChevronUp /> : <FiChevronDown />}
+								</span>
+							</div>
+							<div className='TechCard__Text'>
+								{TechImages.techDescriptions.typescriptRxjs}
+							</div>
+						</div>
+					</div>
+					{/* TypeScript & RxJS */}
+					{/* Frameworks */}
+					<div className='TechCard'>
+						<span className='TechCard__Tittle'>Frameworks</span>
+						<div className='TechCard__WrapperImgs'>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.reactSVG}
+								alt='react_img'
+							/>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.angularSVG}
+								alt='angular_img'
+							/>
+						</div>
+						<div
+							className={`TechCard__DropDown ${
+								openSection === 'FT' ? 'open' : ''
+							}`}
+							onClick={() => toggleSection('FT')}
+						>
+							<div className='TechCard__DropDownTittle'>
+								<span className='summary'>React & Angular</span>
+								<span className='TechCard__ArrowIcon'>
+									{openSection === 'FT' ? <FiChevronUp /> : <FiChevronDown />}
+								</span>
+							</div>
+							<div className='TechCard__Text'>
+								{TechImages.techDescriptions.reactAngular}
+							</div>
+						</div>
+					</div>
+					{/* Frameworks */}
+					{/* CMS */}
+					<div className='TechCard'>
+						<span className='TechCard__Tittle'>CMS</span>
+						<div className='TechCard__WrapperImgs'>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.wordpressSVG}
+								alt='wordpress_img'
+							/>
+							<img
+								className='TechCard__Img'
+								src={TechImages.techBuild.wooCommerceSVG}
+								alt='wooCommerce_img'
+							/>
+						</div>
+						<div
+							className={`TechCard__DropDown ${
+								openSection === 'CT' ? 'open' : ''
+							}`}
+							onClick={() => toggleSection('CT')}
+						>
+							<div className='TechCard__DropDownTittle'>
+								<span className='summary'>Wordpress & WooCommerce</span>
+								<span className='TechCard__ArrowIcon'>
+									{openSection === 'CT' ? <FiChevronUp /> : <FiChevronDown />}
+								</span>
+							</div>
+							<div className='TechCard__Text'>
+								{TechImages.techDescriptions.wordpressWoocommerce}
+							</div>
+						</div>
+					</div>
+					{/* CMS */}
 				</div>
-				{/* Basic Technologies */}
-				{/* CSS Libraries */}
-				<div className='TechCard'>
-					<span className='TechCard__Tittle'>CSS Libraries</span>
-					<div className='TechCard__WrapperImgs'>
-						<img
-							className='TechCard__Img'
-							src={TechImages.techBuild.bootstrapSVG}
-							alt='bootstrap_img'
-						/>
-						<img
-							className='TechCard__Img'
-							src={TechImages.techBuild.tailwindSvg}
-							alt='tailwind_img'
-						/>
-					</div>
-					<div
-						className={`TechCard__DropDown ${
-							openSection === 'CSST' ? 'open' : ''
-						}`}
-						onClick={() => toggleSection('CSST')}
-					>
-						<div className='TechCard__DropDownTittle'>
-							<span className='summary'>Bootstrap & Taildwind</span>
-							<span className='TechCard__ArrowIcon'>
-								{openSection === 'CSST' ? <FiChevronUp /> : <FiChevronDown />}
-							</span>
-						</div>
-						<div className='TechCard__Text'>
-							{TechImages.techDescriptions.tailwindBootstrap}
-						</div>
-					</div>
-				</div>
-				{/* CSS Libraries */}
-				{/* Pre-Processors */}
-				<div className='TechCard'>
-					<span className='TechCard__Tittle'>Pre-Processors</span>
-					<div className='TechCard__WrapperImgs'>
-						<img
-							className='TechCard__Img'
-							src={TechImages.techBuild.sassSVG}
-							alt='sass_img'
-						/>
-						<img
-							className='TechCard__Img'
-							src={TechImages.techBuild.lessSvg}
-							alt='less_img'
-						/>
-					</div>
-					<div
-						className={`TechCard__DropDown ${
-							openSection === 'TPT' ? 'open' : ''
-						}`}
-						onClick={() => toggleSection('TPT')}
-					>
-						<div className='TechCard__DropDownTittle'>
-							<span className='summary'>SASS & LESS</span>
-							<span className='TechCard__ArrowIcon'>
-								{openSection === 'TPT' ? <FiChevronUp /> : <FiChevronDown />}
-							</span>
-						</div>
-						<div className='TechCard__Text'>
-							{TechImages.techDescriptions.sassLess}
-						</div>
-					</div>
-				</div>
-				{/* Pre-Processors */}
-				{/* Frameworks */}
-				<div className='TechCard'>
-					<span className='TechCard__Tittle'>Frameworks</span>
-					<div className='TechCard__WrapperImgs'>
-						<img
-							className='TechCard__Img'
-							src={TechImages.techBuild.reactSVG}
-							alt='react_img'
-						/>
-						<img
-							className='TechCard__Img'
-							src={TechImages.techBuild.angularSVG}
-							alt='angular_img'
-						/>
-					</div>
-					<div
-						className={`TechCard__DropDown ${
-							openSection === 'FT' ? 'open' : ''
-						}`}
-						onClick={() => toggleSection('FT')}
-					>
-						<div className='TechCard__DropDownTittle'>
-							<span className='summary'>React & Angular</span>
-							<span className='TechCard__ArrowIcon'>
-								{openSection === 'FT' ? <FiChevronUp /> : <FiChevronDown />}
-							</span>
-						</div>
-						<div className='TechCard__Text'>
-							{TechImages.techDescriptions.reactAngular}
-						</div>
-					</div>
-				</div>
-				{/* Frameworks */}
-				{/* CMS */}
-				<div className='TechCard'>
-					<span className='TechCard__Tittle'>CMS</span>
-					<div className='TechCard__WrapperImgs'>
-						<img
-							className='TechCard__Img'
-							src={TechImages.techBuild.wordpressSVG}
-							alt='wordpress_img'
-						/>
-						<img
-							className='TechCard__Img'
-							src={TechImages.techBuild.wooCommerceSVG}
-							alt='wooCommerce_img'
-						/>
-					</div>
-					<div
-						className={`TechCard__DropDown ${
-							openSection === 'CT' ? 'open' : ''
-						}`}
-						onClick={() => toggleSection('CT')}
-					>
-						<div className='TechCard__DropDownTittle'>
-							<span className='summary'>Wordpress & WooCommerce</span>
-							<span className='TechCard__ArrowIcon'>
-								{openSection === 'CT' ? <FiChevronUp /> : <FiChevronDown />}
-							</span>
-						</div>
-						<div className='TechCard__Text'>
-							{TechImages.techDescriptions.wordpressWoocommerce}
-						</div>
-					</div>
-				</div>
-				{/* CMS */}
 			</section>
 		</>
 	);
--- src/components/Technologies/TechnologiesData.js
+++ src/components/Technologies/TechnologiesData.js
@@ -1,7 +1,7 @@
 import htmlSvgIMG from './../../assets/img/techSVG/html5-color.svg';
 import cssSvgIMG from './../../assets/img/techSVG/css3-color.svg';
 import sassSvgIMG from './../../assets/img/techSVG/sass-color.svg';
-import lessSvgIMG from './../../assets/img/techSVG/less.svg'
+import lessSvgIMG from './../../assets/img/techSVG/less.svg';
 import javaScriptSvgIMG from './../../assets/img/techSVG/javascript-color.svg';
 import typeScriptSvgIMG from './../../assets/img/techSVG/typescript-color.svg';
 import angularSvgIMG from './../../assets/img/techSVG/angular-color.svg';
@@ -16,7 +16,7 @@ const TechImages = {
 	techBuild: {
 		htmlSVG: htmlSvgIMG,
 		cssSVG: cssSvgIMG,
-        sassSVG: sassSvgIMG,
+		sassSVG: sassSvgIMG,
 		lessSvg: lessSvgIMG,
 		javaScriptSvg: javaScriptSvgIMG,
 		angularSVG: angularSvgIMG,
@@ -30,16 +30,12 @@ const TechImages = {
 	},
 
 	techDescriptions: {
-		htmlCssJs:
-			'My experience in HTML allows me to effectively structure web content. Through CSS, I apply responsive and attractive designs, and with JavaScript, I manage dynamic interactions and API manipulation in my web projects.',
-		tailwindBootstrap:
-			'I use Bootstrap to quickly build responsive user interfaces with its predefined components. On the other hand, I use Tailwind when I need granular control over the styling, as it provides a wide range of utility classes to build custom layouts.',
-		sassLess:
-			'I use Sass and Less in my web development because they offer an advanced syntax for CSS, allowing the use of variables, functions, and mixins, which simplifies and organizes the code. These preprocessors facilitate code reuse and maintenance, especially in large projects, by enabling a more concise and modular writing of style sheets',
-		reactAngular:
-			'I have a solid understanding of both React and Angular. With React, I can create dynamic and efficient user interfaces, leveraging its component model, virtual DOM reconciliation, and the use of hooks. In Angular, I excel in building large-scale single-page applications, using its comprehensive framework based on TypeScript, with a deep understanding of modules, components, services, and routing.',
-		wordpressWoocommerce:
-			'I have experience with the WordPress content management platform, enabling me to create attractive and functional websites. Additionally, I am familiar with WooCommerce, which allows me to build robust and customizable online stores, including product setup, payment methods, and shipping management.',
+		htmlCssJs: `My experience in HTML allows me to effectively structure web content. Through CSS, I apply responsive and attractive designs, and with JavaScript, I manage dynamic interactions and API manipulation in my web projects.`,
+		tailwindBootstrap: `I use Bootstrap to quickly build responsive user interfaces with its predefined components. On the other hand, I use Tailwind when I need granular control over the styling, as it provides a wide range of utility classes to build custom layouts.`,
+		sassLess: `I use Sass and Less in my web development because they offer an advanced syntax for CSS, allowing the use of variables, functions, and mixins, which simplifies and organizes the code. These preprocessors facilitate code reuse and maintenance, especially in large projects, by enabling a more concise and modular writing of style sheets`,
+		typescriptRxjs: `I use TypeScript and RxJS in web development projects to leverage TypeScript's static typing and compile-time error detection, and to efficiently manage asynchronous data streams and events through the reactive programming facilitated by RxJS, thereby enhancing the robustness and maintainability of the code.`,
+		reactAngular: `I have a solid understanding of both React and Angular. With React, I can create dynamic and efficient user interfaces, leveraging its component model, virtual DOM reconciliation, and the use of hooks. In Angular, I excel in building large-scale single-page applications, using its comprehensive framework based on TypeScript, with a deep understanding of modules, components, services, and routing.`,
+		wordpressWoocommerce: `I have experience with the WordPress content management platform, enabling me to create attractive and functional websites. Additionally, I am familiar with WooCommerce, which allows me to build robust and customizable online stores, including product setup, payment methods, and shipping management.`,
 	},
 };
 export default TechImages;
