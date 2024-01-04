d7832a6ac83d572ffe3c9487bcb33167a2dda7b6
Marc1515 <meq.1515@gmail.com>
2024-01-04 19:11:51 +1100

    All the project has setted between 1000px

--- src/components/Contact/ContactComponent.jsx
+++ src/components/Contact/ContactComponent.jsx
@@ -8,55 +8,57 @@ const ContactComponent = () => {
 		<>
 			<div className='spacer layer4'></div>
 			<section id='contact' className='contactSection'>
-				<div className='contactTitle'>
-					<span className='contactTitle__Title'>Contact</span>
-					<span className='contactTitle__Text'>
-						You can contact me directly through any of these links.
-					</span>
-				</div>
-				<div className='contactCards'>
-					<div className='contactCards__Card'>
-						<BsWhatsapp className='contactCards__Icon' />
-						<span className='contactCards__IconName'>WhatsApp</span>
-						<span className='contactCards__Info'>0451 565 137</span>
-						<a
-							className='contactCards__Link'
-							href='whatsapp://send?phone=+61451565137'
-							target='_blank'
-							rel='noreferrer'
-						>
-							Send a message
-						</a>
+				<div className='sectionWrapper'>
+					<div className='contactTitle'>
+						<span className='contactTitle__Title'>Contact</span>
+						<span className='contactTitle__Text'>
+							You can contact me directly through any of these links.
+						</span>
 					</div>
-				</div>
-				<div className='contactCards'>
-					<div className='contactCards__Card'>
-						<MdOutlineMail className='contactCards__Icon' />
-						<span className='contactCards__IconName'>Mail</span>
-						<span className='contactCards__Info'>meq.1515@gmail.com</span>
-						<a
-							className='contactCards__Link'
-							href='mailto:meq.1515@gmail.com'
-							target='_blank'
-							rel='noreferrer'
-						>
-							Send a message
-						</a>
+					<div className='contactCards'>
+						<div className='contactCards__Card'>
+							<BsWhatsapp className='contactCards__Icon' />
+							<span className='contactCards__IconName'>WhatsApp</span>
+							<span className='contactCards__Info'>0451 565 137</span>
+							<a
+								className='contactCards__Link'
+								href='whatsapp://send?phone=+61451565137'
+								target='_blank'
+								rel='noreferrer'
+							>
+								Send a message
+							</a>
+						</div>
 					</div>
-				</div>
-				<div className='contactCards'>
-					<div className='contactCards__Card'>
-						<AiFillLinkedin className='contactCards__Icon' />
-						<span className='contactCards__IconName'>LinkedIn</span>
-						<span className='contactCards__Info'>Marc España</span>
-						<a
-							className='contactCards__Link'
-							href='https://www.linkedin.com/in/marc-espa%C3%B1a-833924141/'
-							target='_blank'
-							rel='noreferrer'
-						>
-							Send a message
-						</a>
+					<div className='contactCards'>
+						<div className='contactCards__Card'>
+							<MdOutlineMail className='contactCards__Icon' />
+							<span className='contactCards__IconName'>Mail</span>
+							<span className='contactCards__Info'>meq.1515@gmail.com</span>
+							<a
+								className='contactCards__Link'
+								href='mailto:meq.1515@gmail.com'
+								target='_blank'
+								rel='noreferrer'
+							>
+								Send a message
+							</a>
+						</div>
+					</div>
+					<div className='contactCards'>
+						<div className='contactCards__Card'>
+							<AiFillLinkedin className='contactCards__Icon' />
+							<span className='contactCards__IconName'>LinkedIn</span>
+							<span className='contactCards__Info'>Marc España</span>
+							<a
+								className='contactCards__Link'
+								href='https://www.linkedin.com/in/marc-espa%C3%B1a-833924141/'
+								target='_blank'
+								rel='noreferrer'
+							>
+								Send a message
+							</a>
+						</div>
 					</div>
 				</div>
 			</section>
--- src/components/about/AboutComponent.jsx
+++ src/components/about/AboutComponent.jsx
@@ -11,47 +11,49 @@ const AboutComponent = () => {
 		<>
 			<div className='spacer layer1'></div>
 			<section id='about' className='aboutSection'>
-				<div className='aboutTittleContainer'>
-					<span className='aboutTittleContainer__Tittle'>About Me</span>
-				</div>
-				<div className='textContainer'>
-					<p className='textContainer__Text1'>
-						I began to awaken my passion for web development about 3 years ago.
-						With the arrival of the pandemic, I started practicing with
-						different technologies. I also worked for 1 year with a local
-						company. My role was to create an interface for an electronic locker
-						reservation page.
-					</p>
-					<p className='textContainer__Text2'>
-						CURRENTLY, I STUDY ON MY OWN AND SEEK TO EXPAND MY CAREER
-						OPORTUNITIES IN THE FIELD OF DEVELOPMENT WEB
-					</p>
-				</div>
-				<div className='socialMediaIconsContainer'>
-					<a
-						href='https://github.com/Marc1515'
-						target='_blank'
-						rel='noreferrer'
-						className='socialMediaIconsContainer__icon'
-					>
-						<FontAwesomeIcon icon={faGithub} size='2x' />
-					</a>
-					<a
-						href='https://www.instagram.com/marc_espp/'
-						target='_blank'
-						rel='noreferrer'
-						className='socialMediaIconsContainer__icon'
-					>
-						<FontAwesomeIcon icon={faInstagram} size='2x' />
-					</a>
-					<a
-						href='https://www.linkedin.com/in/marc-espa%C3%B1a-833924141/'
-						target='_blank'
-						rel='noreferrer'
-						className='socialMediaIconsContainer__icon'
-					>
-						<FontAwesomeIcon icon={faLinkedin} size='2x' />
-					</a>
+				<div className='sectionWrapper'>
+					<div className='aboutTittleContainer'>
+						<span className='aboutTittleContainer__Tittle'>About Me</span>
+					</div>
+					<div className='textContainer'>
+						<p className='textContainer__Text1'>
+							I began to awaken my passion for web development about 3 years
+							ago. With the arrival of the pandemic, I started practicing with
+							different technologies. I also worked for 1 year with a local
+							company. My role was to create an interface for an electronic
+							locker reservation page.
+						</p>
+						<p className='textContainer__Text2'>
+							CURRENTLY, I STUDY ON MY OWN AND SEEK TO EXPAND MY CAREER
+							OPORTUNITIES IN THE FIELD OF DEVELOPMENT WEB
+						</p>
+					</div>
+					<div className='socialMediaIconsContainer'>
+						<a
+							href='https://github.com/Marc1515'
+							target='_blank'
+							rel='noreferrer'
+							className='socialMediaIconsContainer__icon'
+						>
+							<FontAwesomeIcon icon={faGithub} size='2x' />
+						</a>
+						<a
+							href='https://www.instagram.com/marc_espp/'
+							target='_blank'
+							rel='noreferrer'
+							className='socialMediaIconsContainer__icon'
+						>
+							<FontAwesomeIcon icon={faInstagram} size='2x' />
+						</a>
+						<a
+							href='https://www.linkedin.com/in/marc-espa%C3%B1a-833924141/'
+							target='_blank'
+							rel='noreferrer'
+							className='socialMediaIconsContainer__icon'
+						>
+							<FontAwesomeIcon icon={faLinkedin} size='2x' />
+						</a>
+					</div>
 				</div>
 			</section>
 		</>
