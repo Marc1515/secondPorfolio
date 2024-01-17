0772e16129a760f8eecdf74751845f2561ed0137
Marc1515 <meq.1515@gmail.com>
2024-01-17 16:53:14 +1100

    New accent color to help to stand out some components

--- src/styles/material/themeProvider.js
+++ src/styles/material/themeProvider.js
@@ -1,17 +1,20 @@
-// theme.js
 import { createTheme } from '@mui/material/styles';
+import './../variables/_colors.scss';
+
+const getCssColor = (color) =>
+	getComputedStyle(document.documentElement).getPropertyValue(color);
 
 const theme = createTheme({
 	components: {
 		MuiPaginationItem: {
 			styleOverrides: {
 				root: {
-					color: '#FF0000', // reemplaza esto con el color que desees
+					color: getCssColor('--primary-background-color'), // reemplaza esto con el color que desees
 				},
 				page: {
 					'&.Mui-selected': {
-						backgroundColor: 'tuColorParaBotonActivo', // Cambia esto por el color que prefieras para el botón activo
-						color: 'tuColorDeTextoParaBotonActivo', // Cambia esto por el color de texto que prefieras para el botón activo
+						backgroundColor: '#FFF', // Cambia esto por el color que prefieras para el botón activo
+						color: '#000', // Cambia esto por el color de texto que prefieras para el botón activo
 					},
 					'&.Mui-selected:hover': {
 						backgroundColor: 'tuColorParaBotonActivoHover', // Opcional: Cambia esto por el color que prefieras cuando se pasa el mouse sobre el botón activo
