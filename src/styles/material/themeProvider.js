4768cd1d638112c70f1d43174dcd84c9dd6fc318
Marc1515 <meq.1515@gmail.com>
2024-01-17 16:58:12 +1100

    New styles costumized for the pagination

--- src/styles/material/themeProvider.js
+++ src/styles/material/themeProvider.js
@@ -1,23 +1,58 @@
 import { createTheme } from '@mui/material/styles';
 import './../variables/_colors.scss';
+import './../variables/_screens.scss';
 
 const getCssColor = (color) =>
 	getComputedStyle(document.documentElement).getPropertyValue(color);
 
+// Definición de breakpoints
+const breakpoints = {
+	values: {
+		smallPhones: 576,
+		largePhonesTablets: 767,
+		tabletsSmallScreens: 991,
+		screens: 1200,
+	},
+};
+
 const theme = createTheme({
+	breakpoints,
 	components: {
 		MuiPaginationItem: {
 			styleOverrides: {
 				root: {
-					color: getCssColor('--primary-background-color'), // reemplaza esto con el color que desees
+					color: getCssColor('--primary-background-color'),
+
+					fontSize: '1.5rem',
+					height: '3rem',
+					width: '3rem',
+					borderRadius: '50%',
+
+					[`@media (min-width:${breakpoints.values.largePhonesTablets}px)`]: {
+						fontSize: '2rem',
+						height: '3.5rem',
+						minWidth: '3.5rem',
+					},
+
+					[`@media (min-width:${breakpoints.values.tabletsSmallScreens}px)`]: {
+						fontSize: '2.5rem',
+						height: '4rem',
+						minWidth: '4rem',
+					},
+
+					[`@media (min-width:${breakpoints.values.screens}px)`]: {
+						fontSize: '1.5rem',
+						height: '3rem',
+						minWidth: '3rem',
+					},
 				},
 				page: {
 					'&.Mui-selected': {
-						backgroundColor: '#FFF', // Cambia esto por el color que prefieras para el botón activo
-						color: '#000', // Cambia esto por el color de texto que prefieras para el botón activo
+						backgroundColor: getCssColor('--accent-color'),
+						color: getCssColor('--quaternary-background-color'),
 					},
 					'&.Mui-selected:hover': {
-						backgroundColor: 'tuColorParaBotonActivoHover', // Opcional: Cambia esto por el color que prefieras cuando se pasa el mouse sobre el botón activo
+						backgroundColor: getCssColor('--primary-background-color'),
 					},
 				},
 			},
