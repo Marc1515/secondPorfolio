70c25b9f3f45ef6cdda76b36bd27d679c8640afc
Marc1515 <meq.1515@gmail.com>
2024-01-21 17:14:15 +1100

    I've brought the context on the ScrollTopButtomComponent to activate the function which control when the HeaderComponent it has to open

--- src/components/ScrollTopButton/ScrollTopButtonComponent.jsx
+++ src/components/ScrollTopButton/ScrollTopButtonComponent.jsx
@@ -1,11 +1,13 @@
 import { MdKeyboardDoubleArrowUp } from 'react-icons/md';
 
 import './ScrollTopButtonComponent.scss';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
+import { MenuContext } from '../../context/MenuContext';
 
 const ScrollTopButtonComponent = () => {
 	const [isVisible, setIsVisible] = useState(false);
 	const [shouldFadeOut, setShouldFadeOut] = useState(false);
+	const { toggleMenu } = useContext(MenuContext);
 
 	const toggleVisibility = () => {
 		const shouldShow = window.pageYOffset > 250;
@@ -25,6 +27,7 @@ const ScrollTopButtonComponent = () => {
 			top: 0,
 			behavior: 'smooth',
 		});
+		toggleMenu();
 	};
 
 	useEffect(() => {
