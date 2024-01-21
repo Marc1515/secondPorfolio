8096d1ca2785600b823a3d77b5927b2802e2a9da
Marc1515 <meq.1515@gmail.com>
2024-01-21 15:23:28 +1100

    New component with the function to bring the use on the top position when its clicked

---
+++ src/components/ScrollTopButton/ScrollTopButtonComponent.jsx
@@ -0,0 +1,50 @@
+import { MdKeyboardDoubleArrowUp } from 'react-icons/md';
+
+import './ScrollTopButtonComponent.scss';
+import { useEffect, useState } from 'react';
+
+const ScrollTopButtonComponent = () => {
+	const [isVisible, setIsVisible] = useState(false);
+	const [shouldFadeOut, setShouldFadeOut] = useState(false);
+
+	const toggleVisibility = () => {
+		const shouldShow = window.pageYOffset > 250;
+		if (shouldShow) {
+			setIsVisible(true);
+			setShouldFadeOut(false);
+		} else if (isVisible && !shouldShow) {
+			setShouldFadeOut(true);
+			setTimeout(() => {
+				setIsVisible(false);
+			}, 300);
+		}
+	};
+
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', toggleVisibility);
+
+		return () => {
+			window.removeEventListener('scroll', toggleVisibility);
+		};
+	}, [isVisible]);
+
+	return (
+		isVisible && (
+			<button
+				onClick={scrollToTop}
+				className={`scrollTopButton ${shouldFadeOut ? 'fadeOut' : ''}`}
+			>
+				<MdKeyboardDoubleArrowUp />
+			</button>
+		)
+	);
+};
+
+export default ScrollTopButtonComponent;
